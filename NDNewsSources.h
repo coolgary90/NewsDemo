095d81b51238b6faa9807a1af6d9dbfc97c472d7
Amanpreet Singh <you@coolgary90@gmail.com>
2017-02-03 18:03:43 +0530

    Setup project

---
+++ NDNewsSources.h
@@ -0,0 +1,16 @@
+//
+//  NDNewsSources.h
+//  NewsDemo
+//
+//  Created by Amanpreet Singh on 03/02/17.
+//  Copyright © 2017 Amanpreet Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NDNewsSources : UIViewController
+
+@property(weak, nonatomic) IBOutlet UICollectionView* collectionView;
+
+
+@end
