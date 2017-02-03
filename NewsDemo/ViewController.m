d90b99a1c90bc11cc58f75f3ffeb5ab5cfee9064
Amanpreet Singh <you@coolgary90@gmail.com>
2017-02-03 17:33:28 +0530

    Initial Commit

---
+++ NewsDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NewsDemo
+//
+//  Created by Amanpreet Singh on 03/02/17.
+//  Copyright © 2017 Amanpreet Singh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
